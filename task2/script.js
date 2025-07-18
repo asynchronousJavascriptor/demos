105195a536a74e9bc63b4b00455bbf0f776a1e8e
Harsh Sharma <harshu8545@gmail.com>
2025-07-18 12:37:30 +0530

    initial state of task 2

---
+++ task2/script.js
@@ -0,0 +1,7 @@
+const products = [
+  { name: "Laptop", price: 1200 },
+  { name: "Headphones", price: 150 },
+  { name: "Keyboard", price: 80 },
+  { name: "Monitor", price: 300 },
+  { name: "Mouse", price: 40 },
+];
